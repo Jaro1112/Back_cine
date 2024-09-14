f77b4913aa5f6d87e0e9ae579d8053740f1055c2
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-09-14 04:12:19 -0500

    conexion front

--- src/main/java/com/CineJaro/CineJaroApplication.java
+++ src/main/java/com/CineJaro/CineJaroApplication.java
@@ -3,7 +3,7 @@ package com.CineJaro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.CineJaro")
 public class CineJaroApplication {
     public static void main(String[] args) {
         SpringApplication.run(CineJaroApplication.class, args);
